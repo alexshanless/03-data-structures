a18627ef12733ed6398d72dee0e829965caeac31
Alex Bilba <alex.shanless@gmail.com>
2024-04-28 10:55:14 -0700

    done

--- src/webflow/index.js
+++ src/webflow/index.js
@@ -1,12 +1,12 @@
 // Elements
-const todoList = document.querySelector("#todo-list");
-const todoInput = document.querySelector("#todo-input-field");
-const todoTemplate = document.querySelector("#todo-item");
-const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector('#todo-list');
+const todoInput = document.querySelector('#todo-input-field');
+const todoTemplate = document.querySelector('#todo-item');
+const todoForm = document.querySelector('#todo-form');
 
 const todos = [
   {
-    text: "Debug coffee machine",
+    text: 'Debug coffee machine',
     completed: false, // Status: Still trying to reproduce the error after the first cup...
   },
   {
@@ -14,11 +14,11 @@ const todos = [
     completed: false, // Because who needs version control for files?
   },
   {
-    text: "Submit my PR to Github so Bae can grade it",
+    text: 'Submit my PR to Github so Bae can grade it',
     completed: false, // I hope I didn't forget anything...
   },
   {
-    text: "Figure why the cat keeps rebooting my laptop",
+    text: 'Figure why the cat keeps rebooting my laptop',
     completed: true, // Spoiler: Spline made the keyboard a nice warm place to sit down!
   },
 ];
@@ -26,6 +26,19 @@ const todos = [
 // Clears the todo list and renders the todos
 function renderTodos() {
   // Your code here
+  todoList.innerHTML = '';
+
+  todos.forEach(todo => {
+    const todoClone = todoTemplate.cloneNode(true);
+    const todoText = todoClone.querySelector('.todo-text');
+    todoText.textContent = todo.text;
+    if (todo.completed) {
+      todoText.classList.add('completed');
+    }
+
+    todoClone.addEventListener('click', toggleCompleted);
+    todoList.appendChild(todoClone);
+  });
 }
 
 // Takes the value from the input field and adds a new todo to the todos array
@@ -37,12 +50,24 @@ function addTodo(e) {
   e.stopPropagation();
 
   // Your code here
+  const inputValue = todoInput.value;
+  const newTodo = {
+    text: inputValue,
+    completed: false,
+  };
+  todos.push(newTodo);
+  renderTodos();
+  todoInput.value = '';
 }
 
 function toggleCompleted(e) {
   // Your code here
+  const todoText = e.target;
+  todoText.classList.toggle('completed');
 }
 
 // Execution
-todoForm.addEventListener("submit", addTodo);
+todoForm.addEventListener('submit', addTodo);
 renderTodos();
+
+console.log('hi');
